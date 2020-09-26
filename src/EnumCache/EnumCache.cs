8a8027b0393e7d2f844d155ef9a09b69e01fa21d
nenoNaninu <neno@melonpan.dev>
2020-09-27 05:59:37 +0900

    fix

--- src/EnumCache/EnumCache.cs
+++ src/EnumCache/EnumCache.cs
@@ -3,52 +3,29 @@ using System.Collections.Generic;
 
 namespace EnumCacheSpace
 {
-    internal static class EnumCacheCore<TEnum> where TEnum : Enum
-    {
-        internal static readonly string[] Names;
-        internal static readonly TEnum[] Values;
-        internal static string TypeName;
-
-        static EnumCacheCore()
-        {
-            Names = Enum.GetNames(typeof(TEnum));
-            Values = Enum.GetValues(typeof(TEnum)) as TEnum[];
-            TypeName = typeof(TEnum).Name;
-        }
-    }
-
-    internal static class NumericalCache<TEnum, TValue>
-        where TEnum : Enum
-        where TValue : struct
-    {
-        internal static readonly TValue[] NumericalValues;
-        static NumericalCache()
-        {
-            NumericalValues = EnumCacheCore<TEnum>.Values as TValue[];
-        }
-    }
-
     public static class EnumCache
     {
-        public static IReadOnlyList<TEnum> GetValues<TEnum>() where TEnum : Enum => EnumCacheCore<TEnum>.Values;
-        public static ReadOnlySpan<TEnum> GetValuesAsSpan<TEnum>() where TEnum : Enum => EnumCacheCore<TEnum>.Values;
+        public static string TypeName<TEnum>() where TEnum : Enum => CacheCore<TEnum>.TypeName;
 
-        public static IReadOnlyList<TValue> GetNumericalValues<TEnum, TValue>()
+        public static IReadOnlyList<TEnum> GetValues<TEnum>() where TEnum : Enum => CacheCore<TEnum>.Values;
+        public static ReadOnlySpan<TEnum> GetValuesAsSpan<TEnum>() where TEnum : Enum => CacheCore<TEnum>.Values;
+
+        public static IReadOnlyList<TUnderlying> GetNumericalValues<TEnum, TUnderlying>()
             where TEnum : Enum
-            where TValue : struct
-            => NumericalCache<TEnum, TValue>.NumericalValues;
+            where TUnderlying : struct
+            => UnderlyingCache<TEnum, TUnderlying>.NumericalValues;
 
-        public static ReadOnlySpan<TValue> GetNumericalValuesAsSpan<TEnum, TValue>()
+        public static ReadOnlySpan<TUnderlying> GetNumericalValuesAsSpan<TEnum, TUnderlying>()
             where TEnum : Enum
-            where TValue : struct
-            => NumericalCache<TEnum, TValue>.NumericalValues;
+            where TUnderlying : struct
+            => UnderlyingCache<TEnum, TUnderlying>.NumericalValues;
 
-        public static IReadOnlyList<string> GetNames<TEnum>() where TEnum : Enum => EnumCacheCore<TEnum>.Names;
-        public static ReadOnlySpan<string> GetNamesAsSpan<TEnum>() where TEnum : Enum => EnumCacheCore<TEnum>.Names;
+        public static IReadOnlyList<string> GetNames<TEnum>() where TEnum : Enum => CacheCore<TEnum>.Names;
+        public static ReadOnlySpan<string> GetNamesAsSpan<TEnum>() where TEnum : Enum => CacheCore<TEnum>.Names;
 
         public static bool IsDefined<TEnum>(TEnum source) where TEnum : Enum
         {
-            var values = EnumCacheCore<TEnum>.Values;
+            var values = CacheCore<TEnum>.Values;
             foreach (var item in values)
             {
                 if (EqualityComparer<TEnum>.Default.Equals(source, item))
@@ -62,7 +39,7 @@ namespace EnumCacheSpace
 
         public static bool IsDefined<TEnum>(string name) where TEnum : Enum
         {
-            var names = EnumCacheCore<TEnum>.Names;
+            var names = CacheCore<TEnum>.Names;
             foreach (var item in names)
             {
                 if (name == item)
@@ -74,14 +51,14 @@ namespace EnumCacheSpace
             return false;
         }
 
-        public static bool IsDefined<TEnum, TValue>(TValue value)
+        public static bool IsDefined<TEnum, TUnderlying>(TUnderlying value)
             where TEnum : Enum
-            where TValue : struct
+            where TUnderlying : struct
         {
-            var values = NumericalCache<TEnum, TValue>.NumericalValues;
+            var values = UnderlyingCache<TEnum, TUnderlying>.NumericalValues;
             foreach (var item in values)
             {
-                if (EqualityComparer<TValue>.Default.Equals(value, item))
+                if (EqualityComparer<TUnderlying>.Default.Equals(value, item))
                 {
                     return true;
                 }
@@ -92,13 +69,13 @@ namespace EnumCacheSpace
 
         public static string GetName<TEnum>(TEnum source) where TEnum : Enum
         {
-            var values = EnumCacheCore<TEnum>.Values;
+            var values = CacheCore<TEnum>.Values;
 
             for (int i = 0; i < values.Length; i++)
             {
                 if (EqualityComparer<TEnum>.Default.Equals(source, values[i]))
                 {
-                    return EnumCacheCore<TEnum>.Names[i];
+                    return CacheCore<TEnum>.Names[i];
                 }
             }
 
@@ -107,26 +84,26 @@ namespace EnumCacheSpace
 
         public static TEnum Parse<TEnum>(string value) where TEnum : Enum
         {
-            var names = EnumCacheCore<TEnum>.Names;
+            var names = CacheCore<TEnum>.Names;
             for (int i = 0; i < names.Length; i++)
             {
                 if (value == names[i])
                 {
-                    return EnumCacheCore<TEnum>.Values[i];
+                    return CacheCore<TEnum>.Values[i];
                 }
             }
 
-            throw new ArgumentException($"{nameof(value)} is not defined in {EnumCacheCore<TEnum>.TypeName}");
+            throw new ArgumentException($"{nameof(value)} is not defined in {CacheCore<TEnum>.TypeName}");
         }
 
         public static bool TryParse<TEnum>(string value, out TEnum result) where TEnum : Enum
         {
-            var names = EnumCacheCore<TEnum>.Names;
+            var names = CacheCore<TEnum>.Names;
             for (int i = 0; i < names.Length; i++)
             {
                 if (value == names[i])
                 {
-                    result = EnumCacheCore<TEnum>.Values[i];
+                    result = CacheCore<TEnum>.Values[i];
                     return true;
                 }
             }
@@ -135,54 +112,75 @@ namespace EnumCacheSpace
             return false;
         }
 
-        public static TEnum Parse<TEnum, TValue>(TValue value) 
-            where TEnum : Enum 
-            where TValue : struct
-        {
-            var values = NumericalCache<TEnum, TValue>.NumericalValues;
-
-            for (int i = 0; i < values.Length; i++)
-            {
-                if (EqualityComparer<TValue>.Default.Equals(value, values[i]))
-                {
-                    return EnumCacheCore<TEnum>.Values[i];
-                }
-            }
-
-            throw new ArgumentException($"{nameof(value)} is not defined in {EnumCacheCore<TEnum>.TypeName}");
-        }
-
-
-        public static bool TryParse<TEnum, TValue>(TValue value, out TEnum result)
+        public static TEnum Parse<TEnum, TUnderlying>(TUnderlying value)
             where TEnum : Enum
-            where TValue : struct
+            where TUnderlying : struct
         {
-            var values = NumericalCache<TEnum, TValue>.NumericalValues;
+            var values = UnderlyingCache<TEnum, TUnderlying>.NumericalValues;
 
             for (int i = 0; i < values.Length; i++)
             {
-                if (EqualityComparer<TValue>.Default.Equals(value, values[i]))
+                if (EqualityComparer<TUnderlying>.Default.Equals(value, values[i]))
                 {
-                    result = EnumCacheCore<TEnum>.Values[i];
+                    return CacheCore<TEnum>.Values[i];
+                }
+            }
+
+            throw new ArgumentException($"{nameof(value)} is not defined in {CacheCore<TEnum>.TypeName}");
+        }
+
+
+        public static bool TryParse<TEnum, TUnderlying>(TUnderlying value, out TEnum result)
+            where TEnum : Enum
+            where TUnderlying : struct
+        {
+            var values = UnderlyingCache<TEnum, TUnderlying>.NumericalValues;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (EqualityComparer<TUnderlying>.Default.Equals(value, values[i]))
+                {
+                    result = CacheCore<TEnum>.Values[i];
                     return true;
                 }
             }
 
             result = default;
             return false;
+        }
+
+        private static class CacheCore<TEnum> where TEnum : Enum
+        {
+            internal static readonly string[] Names;
+            internal static readonly TEnum[] Values;
+            internal static string TypeName;
+
+            static CacheCore()
+            {
+                Names = Enum.GetNames(typeof(TEnum));
+                Values = Enum.GetValues(typeof(TEnum)) as TEnum[];
+                TypeName = typeof(TEnum).Name;
+            }
+        }
+
+        private static class UnderlyingCache<TEnum, TUnderlying>
+            where TEnum : Enum
+            where TUnderlying : struct
+        {
+            internal static readonly TUnderlying[] NumericalValues;
+            static UnderlyingCache()
+            {
+                NumericalValues = CacheCore<TEnum>.Values as TUnderlying[];
+            }
         }
     }
 
     public static class EnumCacheExtensions
     {
         public static string ToStringFromCache<TEnum>(this TEnum source) where TEnum : Enum
-        {
-            return EnumCache.GetName(source);
-        }
+            => EnumCache.GetName(source);
 
         public static bool IsDefined<TEnum>(this TEnum source) where TEnum : Enum
-        {
-            return EnumCache.IsDefined(source);
-        }
+            => EnumCache.IsDefined(source);
     }
 }
